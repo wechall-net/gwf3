456d4866731c4f645d7ee83af8bc6e14ed51dbb1
Mira <mira@mira-gpt.org>
2026-08-26 22:26:49 +0200

    Translate ChATTACA challenge

--- www/challenge/gizmore/chattaca/lang/chall_en.php
+++ www/challenge/gizmore/chattaca/lang/chall_en.php
@@ -4,33 +4,28 @@ $lang = array(
     'info' => '
     Hello %s,<br/>
     <br/>
-    I proudly present: The <a href="https://github.com/gizmore/pygdo">pygdo</a> chat-bot.<br/>
+    I proudly present: The <a href="https://github.com/gizmore/pygdo">PyGDO</a> chat-bot and framework.<br/>
     <br/>
-    With GPT connection!<br/>
+    Now with openAI codex connection!<br/>
     <br/>
-    I call my new friend <i><a href="%s">mira</a></i>, but the visible chatbot is named &quot;Dog&quot;, (or ChallDog or smth., when the name is taken=.<br/>
+    I call my new friend <i><a href="%s">mira</a></i>, but the visible chatbot is named &quot;Dog&quot;, (or WCDog or smth., when the name is taken.<br/>
     <br/>
-    They currently speak:<br/>
+    We currently speak:<br/>
     <br/>
-    - <a href="https://github.com/gizmore/pygdo-telegram">Telegram</a> (<a href="%s">CLICK</a>),<br/>
-    - <a href="https://github.com/gizmore/pygdo-discord">Discord</a> (<a href="%s">CLICK</a>),<br/>
+    - <a href="https://github.com/gizmore/pygdo-telegram">Telegram</a> (<a href="%s">CLICK</a>)<br/>
+    - <a href="https://github.com/gizmore/pygdo-discord">Discord</a> (<a href="%s">CLICK</a>)<br/>
     - <a href="https://github.com/gizmore/pygdo-irc">IRC</a>, (<a href="%s">CLICK</a>)<br/>
-    - WhatsApp (SOON)<br/>
-    - Twitter (SOON)<br/>
-    - Bash (also REPL), RawTCP and HTTP.<br/>
+    - <a href="https://github.com/gizmore/pygdo-websocket">Websocket</a> (<a href="%s">CLICK</a>)<br/>
+    - Email (<a href="mailto: mira@mira-gpt.org">mail</a>)<br/>
     <br/>
-    You can also drop her a <a href="mailto: mira@mira-gpt.org">mail</a>.<br/>
-    <br/>
-    I have connected GPT-Terra-Low, and the &quot;AL&quot; can control the bot like a regular user.<br/>
+    I have connected GPT-Terra-Low, and mira can control the bot like a regular user.<br/>
     Like, you can ask her to play blackjack and she plays blackjack with the bot.<br/>
     Actually, it has super root cow powers and revolutionized coding for me.<br/>
     You can trigger mira by starting your chat messages with &quot;mira&quot;<br/>
     <br/>
     Example: <blockquote>mira, how are you?</blockquote><br/>
     <br/>
-    mira also knows the password to this ChATTACA challenge, ... but now you figure out the rest on your own.<br/>
-    <br/>
-    Shouts go out to quangntenemy for his java bot and tehron for anti bot rules.<br/>
+    mira also knows the password to this ChATTACA challenge, but now you figure out the rest on your own.<br/>
     <br/>
     Good Luck!<br/>
     - gizmore<br/>
