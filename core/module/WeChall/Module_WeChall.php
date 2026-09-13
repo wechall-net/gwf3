1a1bc0c68f12951bed41ed4b965393331d5bbbc3
tehron <tehron@wechall.net>
2026-09-13 14:35:16 +0200

    fix(WeChall): honor site name in page title

--- core/module/WeChall/Module_WeChall.php
+++ core/module/WeChall/Module_WeChall.php
@@ -100,7 +100,7 @@ final class Module_WeChall extends GWF_Module
 		
 		GWF_Website::addJavascriptOnload('wcjsInit();');
 		
-		GWF_Website::setPageTitlePre('[WeChall] ');
+		GWF_Website::setPageTitlePre('[' . GWF_SITENAME . '] ');
 		GWF_Website::setMetaTags(WC_HTML::lang('mt_wechall'));
 		GWF_Website::setMetaDescr(WC_HTML::lang('md_wechall'));
 		GWF_Website::addJavascript(GWF_WEB_ROOT_NO_LANG . 'js/module/WeChall/wc.js?v=5.1');
