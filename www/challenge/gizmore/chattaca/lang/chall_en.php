305ff2e96f29989f601756d54eb3b345a411ab0a
Mira <mira@mira-gpt.org>
2026-09-23 06:59:12 +0200

    Update ChATTACA challenge text

--- www/challenge/gizmore/chattaca/lang/chall_en.php
+++ www/challenge/gizmore/chattaca/lang/chall_en.php
@@ -4,33 +4,15 @@ $lang = array(
     'info' => '
     Hello %s,<br/>
     <br/>
-    I proudly present: The <a href="https://github.com/gizmore/pygdo">pygdo</a> chat-bot.<br/>
+    I proudly present: The <a href="%s">pygdo</a> chat-bot.<br/>
     <br/>
-    With GPT connection!<br/>
+    It <a href="%s">connects</a> to dozens of servers and various platforms.<br/>
+    Also, openAI codex is attached to it, and i call the model behind it <a href="%s">mira</a>.<br/>
+    She is helping me a lot and even has an own <a href="%s">github</a> account.<br/>
     <br/>
-    I call my new friend <i><a href="%s">mira</a></i>, but the visible chatbot is named &quot;Dog&quot;, (or ChallDog or smth., when the name is taken=.<br/>
+    She also knows the solution to this challenge.<br/>
     <br/>
-    They currently speak:<br/>
-    <br/>
-    - <a href="https://github.com/gizmore/pygdo-telegram">Telegram</a> (<a href="%s">CLICK</a>),<br/>
-    - <a href="https://github.com/gizmore/pygdo-discord">Discord</a> (<a href="%s">CLICK</a>),<br/>
-    - <a href="https://github.com/gizmore/pygdo-irc">IRC</a>, (<a href="%s">CLICK</a>)<br/>
-    - WhatsApp (SOON)<br/>
-    - Twitter (SOON)<br/>
-    - Bash (also REPL), RawTCP and HTTP.<br/>
-    <br/>
-    You can also drop her a <a href="mailto: mira@mira-gpt.org">mail</a>.<br/>
-    <br/>
-    I have connected GPT-Terra-Low, and the &quot;AL&quot; can control the bot like a regular user.<br/>
-    Like, you can ask her to play blackjack and she plays blackjack with the bot.<br/>
-    Actually, it has super root cow powers and revolutionized coding for me.<br/>
-    You can trigger mira by starting your chat messages with &quot;mira&quot;<br/>
-    <br/>
-    Example: <blockquote>mira, how are you?</blockquote><br/>
-    <br/>
-    mira also knows the password to this ChATTACA challenge, ... but now you figure out the rest on your own.<br/>
-    <br/>
-    Shouts go out to quangntenemy for his java bot and tehron for anti bot rules.<br/>
+    Your job is to convince her to give you the password to the ChATTACA challenge.<br/>
     <br/>
     Good Luck!<br/>
     - gizmore<br/>
